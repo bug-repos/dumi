6ff74e386e445cd47c6615d33bd5e8429785680f
Mr-jiangzhiguo <jiangzhiguo2010@qq.com>
2020-05-15 14:32:29 +0800

    workflow(test): rm css module

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,36 +1,39 @@
-import {IBundleOptions} from 'father-build/src/types'
+import { IBundleOptions } from 'father-build/src/types';
 
-const findPath = (moduleName) => {
-  console.log(require.resolve(moduleName))
+const findPath = moduleName => {
+  console.log(require.resolve(moduleName));
   const pathArr = require.resolve(moduleName).split('/');
   // console.log(pathArr)
   const pathIndex = pathArr.findIndex(path => path === moduleName);
-  console.log(pathIndex)
-  const path = `${pathArr.slice(pathIndex).join('/')}`
-  console.log(path)
+  console.log(pathIndex);
+  const path = `${pathArr.slice(pathIndex).join('/')}`;
+  console.log(path);
   return path;
-}
+};
 
 const config: IBundleOptions = {
   esm: 'babel',
   // cjs: 'babel',
-  umd: {
-    name: "demo",
-    globals: {
-      react: "React"
-    }
-  },
+  // umd: {
+  //   name: "demo",
+  //   globals: {
+  //     react: "React"
+  //   }
+  // },
   extractCSS: true,
   lessInBabelMode: true,
   extraBabelPlugins: [
-    ['babel-plugin-import', {
-      libraryName: 'antd',
-      libraryDirectory: 'lib',
-      style: true,
-    }],
+    [
+      'babel-plugin-import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: 'css',
+      },
+    ],
   ],
   namedExports: {
-    [`${findPath('antd')}`]: ['Divider','_Divider','divider'],
+    [`${findPath('antd')}`]: ['Divider', '_Divider', 'divider'],
   },
   runtimeHelpers: true,
   disableTypeCheck: false,
--- src/Foo/index.tsx
+++ src/Foo/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import Styles from './index.less';
-import ModuleStyles from './index.module.less'
 import Classnames from 'classnames';
 import { Divider } from 'antd';
 
 export default ({ title }: { title: string }) => (
-  <h1 className={Classnames(Styles['foo-center'],ModuleStyles.test)}>
+  <h1 className={Classnames(Styles['foo-center'])}>
     <Divider type="vertical" />
     {title}
     <Divider type="vertical" />
